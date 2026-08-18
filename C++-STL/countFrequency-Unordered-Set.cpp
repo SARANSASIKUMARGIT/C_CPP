e6199ad94b19f833c21d25397751c97370533d03
Saran_SK <82465280+SARANSASIKUMARGIT@users.noreply.github.com>
2026-08-18 23:58:42 +0530

    Create countFrequency-Unordered-Set.cpp

---
+++ C++-STL/countFrequency-Unordered-Set.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<algorithm>     //contains declaration of find() method
+using namespace std;
+
+                /*    Saran SK     */
+
+/*
+    
+*/
+
+
+int main()
+{
+    int n;
+    unordered_map<int,int> frequencyMap;
+    frequencyMap.reserve(n);
+    int* array=(int*)malloc(n*sizeof(int));
+    if(arr==NULL)
+    {
+        printf("Memory allocation failed. Please Try Again \n");
+        exit(1);
+    }
+    for(int i=0;i<n;++i)
+    {
+        printf("Enter Element %d : ");
+        scanf("%d",array+i);
+    }
+
+    for(int i=0;i<n;++i)
+        frequencyMap[array[i]]++;
+
+    for(int )
+    
+
+
+    return 0;
+}
+
+
