32bebc1af749f9b07f050012026f2dfeb4f36d50
Saran_SK <82465280+SARANSASIKUMARGIT@users.noreply.github.com>
2026-08-19 00:30:41 +0530

    Update countFrequency-Unordered-Set.cpp
    C++ Program to calculate the Frequency of each element in an array using Unordered Map (Hash Table),
        An Unordered Map does not sort elements based on key value, 
        it stores elements based only on the hash value of each key , by mapping the hash value of every key to it's calculated bucket index in the hash table,
        Bucket Index = hash(key) % Bucket_Count

--- C++-STL/countFrequency-Unordered-Set.cpp
+++ C++-STL/countFrequency-Unordered-Set.cpp
@@ -1,36 +1,46 @@
 #include<bits/stdc++.h>
-#include<algorithm>     //contains declaration of find() method
+//#include<algorithm>     //contains declaration of find() method
 using namespace std;
 
                 /*    Saran SK     */
 
 /*
-    
+    C++ Program to calculate the Frequency of each element in an array using Unordered Map (Hash Table),
+    An Unordered Map does not sort elements based on key value, 
+    it stores elements based only on the hash value of each key , by mapping the hash value of every key to it's calculated bucket index in the hash table,
+    Bucket Index = hash(key) % Bucket_Count
 */
 
 
 int main()
 {
     int n;
-    unordered_map<int,int> frequencyMap;
-    frequencyMap.reserve(n);
-    int* array=(int*)malloc(n*sizeof(int));
-    if(arr==NULL)
+    unordered_map<int,int> frequencyMap;                        //declare unordered_map<int,int> 
+    cout<<"Enter the number of elements of the array : ";
+    cin>>n;
+    frequencyMap.reserve(n);                                    //reserving capacity for 'n' number of elements
+    int *array = NULL;
+    try{
+        array=new int[n];                                      //allocating dynamic array at heap memory at runtime 
+    }catch(bad_alloc &e)
     {
-        printf("Memory allocation failed. Please Try Again \n");
-        exit(1);
+        //if exception occured
+        cout<<"Memory allocation failed . Try Again "<<endl;
+        return 1;
     }
-    for(int i=0;i<n;++i)
+    for(int i=0;i<n;++i)                            //inputing array element's value
     {
-        printf("Enter Element %d : ");
-        scanf("%d",array+i);
+        cout<<"Enter Element "<<i<< " : ";
+        cin>>array[i];
     }
 
     for(int i=0;i<n;++i)
-        frequencyMap[array[i]]++;
+        frequencyMap[array[i]]++;                   //incrementing the value of current element (key) in the map , if no key exist before the value is set to 0 by default
 
-    for(int )
-    
+    for(const auto &m : frequencyMap)                   //get key-value pair as reference instead of copying it
+        cout<< "Occurence of "<< m.first << " : " << m.second <<endl;               //printing the key-value(count) pair from the unordered_map by for each loop, by taking each pair one by one
+
+    delete[] array;         //deallocating the dynamic array 
 
 
     return 0;
